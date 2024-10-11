50db8255449eb9ff28c52d368b2c9276ded6182f
xXLucasmorasXx <lucasmoras63@gmail.com>
2024-10-11 17:48:04 -0300

    11/10

--- Banco/tbf.sql
+++
@@ -1,8 +0,0 @@
-create table tb_usuarios(
-id_usuario int primary key,
-usuario varchar(50) not null,
-login varchar(50) not null unique,
-senha varchar(15)not null
-);
-insert into tb_usuarios(id_usuario, usuario, login, senha)
-values(1, 'UsuarioRoot', 'root', '123');
